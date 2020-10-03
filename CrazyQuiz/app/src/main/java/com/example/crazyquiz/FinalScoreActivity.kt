cbba5443c126c3a99ade10f46017c7a8da70d228
HannaRomero <hannaromero2@gmail.com>
2020-10-02 21:30:39 -0500

    Cambios
    ..

--- CrazyQuiz/app/src/main/java/com/example/crazyquiz/FinalScoreActivity.kt
+++ CrazyQuiz/app/src/main/java/com/example/crazyquiz/FinalScoreActivity.kt
@@ -4,19 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_final_score.*
 
 class FinalScoreActivity : AppCompatActivity() {
 
     private lateinit var idpuntos: TextView
     private lateinit var idMenuPrincipal: Button
+    private lateinit var gatoView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_score)
 
+        gatoView = findViewById(R.id.GatoImageView)
         idpuntos = findViewById(R.id. idPuntos)
         idMenuPrincipal = findViewById(R.id.idMenuPrincipal)
+
+    
+
 
         val intent = this.intent
         var porcentaje : Int = intent.getIntExtra("Porcentaje", 0)
@@ -26,6 +34,9 @@ class FinalScoreActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+
+
 
     }
 }
