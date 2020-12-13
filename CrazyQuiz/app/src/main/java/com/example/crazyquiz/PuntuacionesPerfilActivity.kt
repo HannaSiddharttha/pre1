affb154ba9a65fb35f37f0de8fd2c103d0082eaa
Hanna <siddharttha95@gmail.com>
2020-12-13 16:07:11 -0600

    reciclerview 2

--- CrazyQuiz/app/src/main/java/com/example/crazyquiz/PuntuacionesPerfilActivity.kt
+++ CrazyQuiz/app/src/main/java/com/example/crazyquiz/PuntuacionesPerfilActivity.kt
@@ -1,12 +1,16 @@
 package com.example.crazyquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.crazyquiz.modelo.puntuaciones
 import kotlinx.android.synthetic.main.activity_puntuaciones_perfil.*
 
-class PuntuacionesPerfilActivity : AppCompatActivity() {
+class PuntuacionesPerfilActivity : AppCompatActivity(),RecyclerAdapter.OnPuntajeClickListener {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
@@ -19,8 +23,36 @@ class PuntuacionesPerfilActivity : AppCompatActivity() {
 
     }
 
-    private fun setupRecyclerView(){
+    private fun setupRecyclerView() {
         reciclerview.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration((DividerItemDecoration(this,DividerItemDecoration.VERTICAL)))
+        val listPuntaje: List<puntuaciones> = listOf(
+            puntuaciones(
+                "Chirimole",
+                "https://www.tierragamer.com/wp-content/uploads/2019/08/One-Piece-Monkey-D-Luffy-Cosplay-Gato.jpg"
+            ),
+            puntuaciones(
+                "Flerky",
+                "https://i.pinimg.com/originals/cc/c6/da/ccc6da24c9cf69967fd65061433af9ed.jpg"
+            ),
+            puntuaciones(
+                "Bola",
+                "https://i.pinimg.com/originals/65/37/49/65374935e22d777a2d0696466900cead.png"
+            )
+        )
+
+        recyclerView.adapter = RecyclerAdapter(this, listPuntaje,this)
+    }
+
+    override fun onImageClick(imagen: String) {
+     val intent = Intent(this,PerfilDetail::class.java)
+        intent.putExtra("imageUrl", imagen)
+        startActivity(intent)
+
+    }
+
+    override fun onItemClick(nombre: String) {
+        Toast.makeText(this,"Este perfil es de: $nombre", Toast.LENGTH_SHORT).show()
     }
 }
 
--- CrazyQuiz/app/src/main/java/com/example/crazyquiz/RecyclerAdapter.kt
+++ CrazyQuiz/app/src/main/java/com/example/crazyquiz/RecyclerAdapter.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.crazyquiz.base.BaseViewHolder
 import com.example.crazyquiz.modelo.puntuaciones
 import kotlinx.android.synthetic.main.puntaje_row.view.*
 import java.lang.IllegalArgumentException
-import java.security.AccessControlContext
 
-class RecyclerAdapter (private val context: Context,val listaPuntaje:List<puntuaciones>): RecyclerView.Adapter<BaseViewHolder<*>>(){
+class RecyclerAdapter (private val context: Context,val listaPuntaje:List<puntuaciones>,val itemClickListener:OnPuntajeClickListener): RecyclerView.Adapter<BaseViewHolder<*>>(){
+
+    interface OnPuntajeClickListener{
+      fun onImageClick(imagen: String)
+      fun onItemClick(nombre: String)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
         return PuntajeViewHolder(LayoutInflater.from(context).inflate(R.layout.puntaje_row,parent,false))
@@ -28,7 +34,9 @@ class RecyclerAdapter (private val context: Context,val listaPuntaje:List<puntua
 
     inner class PuntajeViewHolder(itemView: View):BaseViewHolder<puntuaciones>(itemView){
         override fun bind(item: puntuaciones, position: Int) {
-            //Glide.with(context).load(item.imagen).into(itemView.img_perfil)
+            itemView.setOnClickListener{itemClickListener.onItemClick(item.nombre)}
+            itemView.img_perfil.setOnClickListener{itemClickListener.onImageClick(item.imagen)}
+            Glide.with(context).load(item.imagen).into(itemView.img_perfil)
             itemView.txt_usuario1.text = item.nombre
         }
     }
