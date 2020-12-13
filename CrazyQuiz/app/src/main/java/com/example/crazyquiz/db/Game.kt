284eb89ad2f93942db1bac72a05a06e488493223
Abelardo Ramírez Fuente <abelardo-0@hotmail.com>
2020-12-12 18:24:10 -0600

    objeto "game" creado

---
+++ CrazyQuiz/app/src/main/java/com/example/crazyquiz/db/Game.kt
@@ -0,0 +1,16 @@
+package com.example.crazyquiz.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "games")
+
+data class Game (
+    @PrimaryKey(autoGenerate = true)
+    val gameId : Int,
+    val userId: Int,
+    val isActive: Boolean,
+    val score: Int,
+    val date: Date
+)
