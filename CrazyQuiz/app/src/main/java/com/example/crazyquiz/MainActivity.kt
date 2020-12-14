a814f154a7280a202d91683d56d0f0d2766de724
Hanna <siddharttha95@gmail.com>
2020-12-14 16:22:35 -0600

    fab_button

--- CrazyQuiz/app/src/main/java/com/example/crazyquiz/MainActivity.kt
+++ CrazyQuiz/app/src/main/java/com/example/crazyquiz/MainActivity.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.facebook.stetho.Stetho
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button_juego: Button
     private lateinit var button_options: Button
     private lateinit var button_puntaje: Button
+    var isOpen = false
 
     private val model: GameModel by viewModels()
 
@@ -27,23 +33,57 @@ class MainActivity : AppCompatActivity() {
 
         //Para poder mostrar la bd en Chrome.
         Stetho.initializeWithDefaults(this)
+        val fabOpen = AnimationUtils.loadAnimation(this, R.anim.fab_open)
+        val fabClosed = AnimationUtils.loadAnimation(this, R.anim.fab_closed)
+        val fabRClockwise = AnimationUtils.loadAnimation(this, R.anim.rotate_clockwise)
+        val fabRAntiClockwise = AnimationUtils.loadAnimation(this, R.anim.rotate_anticlockwise)
 
 
-        button_juego = findViewById(R.id. button_juego)
-        button_options = findViewById(R.id. button_options)
-        button_puntaje = findViewById(R.id. button_puntaje)
 
-        button_options.setOnClickListener{  View ->
-            val intent = Intent(this,OptionsActivity::class.java)
-            startActivity(intent)
-        }
-        button_juego.setOnClickListener{  View ->
-            val intent = Intent(this,QuestionActivity::class.java)
-            startActivity(intent)
-        }
-        button_puntaje.setOnClickListener{  View ->
-            val intent = Intent(this,PuntuacionesPerfilActivity::class.java)
-            startActivity(intent)
+        button_juego = findViewById(R.id.button_juego)
+        button_options = findViewById(R.id.button_options)
+        button_puntaje = findViewById(R.id.button_puntaje)
+
+        fab_button.setOnClickListener {
+            if (isOpen) {
+                edit_button.startAnimation(fabClosed)
+                button_exit.startAnimation(fabClosed)
+                fab_button.startAnimation(fabRAntiClockwise)
+
+                isOpen = false
+            } else {
+                edit_button.startAnimation(fabOpen)
+                button_exit.startAnimation(fabOpen)
+                fab_button.startAnimation(fabRClockwise)
+
+                edit_button.isClickable
+                button_exit.isClickable
+
+
+
+                isOpen = true
+            }
+            edit_button.setOnClickListener { View ->
+                val intent = Intent(this, EditActivity::class.java)
+                startActivity(intent)
+            }
+            button_exit.setOnClickListener { View ->
+                Toast.makeText(
+                    this, "Deseas cerrar sesion?", Toast.LENGTH_LONG
+                ).show()
+            }
+            button_options.setOnClickListener { View ->
+                val intent = Intent(this, OptionsActivity::class.java)
+                startActivity(intent)
+            }
+            button_juego.setOnClickListener { View ->
+                val intent = Intent(this, QuestionActivity::class.java)
+                startActivity(intent)
+            }
+            button_puntaje.setOnClickListener { View ->
+                val intent = Intent(this, PuntuacionesPerfilActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
